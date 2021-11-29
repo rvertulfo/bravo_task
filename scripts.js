213311f239640fb1726e8b3eee4468f7b20a19fc
Ryan Vertulfo <rvertulfo@gmail.com>
2021-11-29 18:09:07 +1100

    commit latest

--- scripts.js
+++ scripts.js
@@ -22,14 +22,14 @@ window.addEventListener('resize', function(event){
 let url = 'https://87197325.blob.core.windows.net/fed-technical-test/events.json';
 const eventList = document.querySelector('.event-list');
 fetch(url)
-.then(res => res.json())
+    .then(res => res.json())
 .then((events) => {
 
     for (var event in events) {
-        var eventImage =  (screen_width > 425) ? events[event].imgUrlDesktop : events[event].imgUrlMobile;
-        var eventImage2x =  (screen_width > 425) ? events[event].imgUrlDesktop2x : events[event].imgUrlMobile2x;
-        eventList.innerHTML += '<div class="event-item"><div class="event-item-content"><div class="event-item-image"><img src="' + eventImage + '"  srcset="' + eventImage + ' 1x, ' + eventImage2x + ' 2x"  /></div><div class="event-item-text"><h3>' + events[event].title + '</h3><p>' + events[event].description + '</p></div></div></div>';
-    }
+    var eventImage =  (screen_width > 425) ? events[event].imgUrlDesktop : events[event].imgUrlMobile;
+    var eventImage2x =  (screen_width > 425) ? events[event].imgUrlDesktop2x : events[event].imgUrlMobile2x;
+    eventList.innerHTML += '<div class="event-item"><div class="event-item-content"><div class="event-item-image"><img src="' + eventImage + '"  srcset="' + eventImage + ' 1x, ' + eventImage2x + ' 2x"  /></div><div class="event-item-text"><h3>' + events[event].title + '</h3><p>' + events[event].description + '</p></div></div></div>';
+}
 
 })
 .catch(err => { throw err });
